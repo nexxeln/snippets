4becc21d9039e6acc6dd4548754b1ec16c0f0f46
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-26 23:44:56 +0530

    +: add types for entry and snippet

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,15 @@
 import { z, defineCollection } from "astro:content";
 
+const snippetSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  tags: z.array(z.string()),
+});
+
+export type Snippet = z.infer<typeof snippetSchema>;
+
 export const collections = {
   snippets: defineCollection({
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      tags: z.array(z.string()),
-    }),
+    schema: snippetSchema,
   }),
 };
