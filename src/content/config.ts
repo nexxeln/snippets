e5f27fffd9ee2dbd23c91fcf7f640df9dc78ab11
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-26 23:47:07 +0530

    *: use infer helper

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,11 @@
 import { z, defineCollection } from "astro:content";
 
-const snippetSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  tags: z.array(z.string()),
-});
-
-export type Snippet = z.infer<typeof snippetSchema>;
-
 export const collections = {
   snippets: defineCollection({
-    schema: snippetSchema,
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()),
+    }),
   }),
 };
